dbd350799fb058e0362570c0164d82def57213a4
Ravi9140 <78776196+Ravi9140@users.noreply.github.com>
2022-01-16 17:24:49 +0530

    Update Crossword.c

--- Crossword.c
+++ Crossword.c
@@ -15,8 +15,8 @@ Dict *root=NULL;
 
 Dict* InsertWords(Dict *root, char *word)
 {
-	  Dict *newWord=(Dict *)malloc(sizeof(Dict));
-	  strcpy(newWord->word, word);
+    Dict *newWord=(Dict *)malloc(sizeof(Dict));
+    strcpy(newWord->word, word);
     newWord->left = NULL;
     newWord->right = NULL;
         
@@ -27,17 +27,17 @@ Dict* InsertWords(Dict *root, char *word)
     else if(strcmp(root->word, word) > 0)
     {
     	root->left = InsertWords(root->left, word);
-	  }
-	  else
-	  {
-		  root->right = InsertWords(root->right, word);
-	  }
-	  return root;
+    }
+    else
+    {
+	root->right = InsertWords(root->right, word);
+    }
+    return root;
 }
 
 void DisplayDictionary(Dict *root)
 {
-	if (root)
+    if (root)
     {
         DisplayDictionary(root->left);
         printf("\n%s", root->word);
@@ -51,42 +51,42 @@ void SuggestWords(Dict *root, char *word)
 	
 	//To reach to node with the matching intial character
 	while(root != NULL)
-  {
-    	 if (root->word[0] == word[0])
-            break;
-       else if (root->word[0] < word[0])
-            root=root->right;
-       else    
-            root=root->left;
-   }
+    	{
+    		if (root->word[0] == word[0])
+           	 	break;
+        	else if (root->word[0] < word[0])
+            		root=root->right;
+       		else    
+            		root=root->left;
+    	}
 		 
-   if(root != NULL)
-   {
-        if (strlen(root->word) == strlen(word) && root->word[0]==word[0])
-        {
-                for(i=0;word[i]!='\0';i++)
-                {
-                    if(word[i]!='_' && word[i]!=root->word[i])  
-                    {
-                    	misMatch=1;
-                    	break;
-  					        }
-                }
-                if(misMatch==0)   //Word does not Match
-                {
-                	wordcount++;
-               		if(wordcount==1)
-               		{
-               			printf("\nSuggested Words :- %s\t",root->word);
-			   		      }
-			   		      if(wordcount>1)
-			   		      {
-			   			      printf("%s\t",root->word);
-			   		      }
-				        }
-		      }
-	        SuggestWords(root->left,word);  //Search In Left Child
-          SuggestWords(root->right,word); //Search In Right Child
+    	if(root != NULL)
+    	{
+        	if (strlen(root->word) == strlen(word) && root->word[0]==word[0])
+        	{
+                	for(i=0;word[i]!='\0';i++)
+                	{
+                    		if(word[i]!='_' && word[i]!=root->word[i])  
+                    		{
+                    			misMatch=1;
+                    			break;
+  				}
+                	}
+                	if(misMatch==0)   //Word does not Match
+			{
+                		wordcount++;
+               			if(wordcount==1)
+               			{
+               				printf("\nSuggested Words :- %s\t",root->word);
+			   	}
+			   	if(wordcount>1)
+			   	{
+			   		printf("%s\t",root->word);
+			   	}
+			}
+		}
+	    	SuggestWords(root->left,word);  //Search In Left Child
+        	SuggestWords(root->right,word); //Search In Right Child
    	}
 }
 
@@ -103,31 +103,31 @@ int main()
 	{
 		root=InsertWords(root,dictionary[i]);
 	}
-    printf("\nWords Stored in Dictionary are:\n ");
-    DisplayDictionary(root);
+    	printf("\nWords Stored in Dictionary are:\n ");
+    	DisplayDictionary(root);
     
-    while(1)
-    {
-    	printf("\n\n============================================================================================");
-    	printf("\nEnter the Word(In Format: 'k_k__k' i.e k for known and _ for unknown)\n(Enter q for Quitting!!)");
-    	printf("\n\nEnter the Crossword: ");
-        scanf("%s",crossword);
+    	while(1)
+    	{
+    		printf("\n\n============================================================================================");
+    		printf("\nEnter the Word(In Format: 'k_k__k' i.e k for known and _ for unknown)\n(Enter q for Quitting!!)");
+    		printf("\n\nEnter the Crossword: ");
+        	scanf("%s",crossword);
         
-        if(crossword[0]=='_')
-        {
-        	printf("\nEnter String having initial Character!!!");
-        	continue;
-		    }
+       		if(crossword[0]=='_')
+        	{
+        		printf("\nEnter String having initial Character!!!");
+        		continue;
+		}
         
-		    if(strcmp(crossword,"q") == 0)
-        	  break;
+		if(strcmp(crossword,"q") == 0)
+        		break;
         
-		    wordcount=0;
-       	SuggestWords(root, crossword);
-        if(wordcount==0)
-        {
-        	printf("\nNo Suggestions!!!");
-		    }
-   }
-   return 0;
+		wordcount=0;
+       		SuggestWords(root, crossword);
+      		if(wordcount==0)
+        	{
+        		printf("\nNo Suggestions!!!");
+		}
+   	}
+   	return 0;
 }
